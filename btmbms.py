cf176ca75cb2a6a77ebaff82d5dbfb4a1010f573
Darigov Research <30328618+darigovresearch@users.noreply.github.com>
2022-02-25 23:52:32 +0000

    feat: Adds initial import_from_status to btmbms.py

--- btmbms.py
+++ btmbms.py
@@ -21,5 +21,30 @@ def initialise():
 	print("Initialisation complete")
 
 
+def import_from_status(file_name):
+	"""import_from_status is code to pull content from a Status.csv file"""
+
+	df = pd.read_csv(file_name)
+	print(df.columns)
+	unique_batteries = df[file_columns[0][0]].unique()
+	print(unique_batteries)
+
+	x = open("Data//"+ file_names[2] + ".csv", "w")
+	temp_columns = file_columns[2]
+	x.write(",".join(temp_columns))
+
+	for j in range(0, len(unique_batteries)):
+
+		if j == 0:
+			x.write("\n" + unique_batteries[j] + "\n")
+		else:
+			x.write(unique_batteries[j] + "\n")
+
+
+	x.close()
+
+
 if __name__ == '__main__':
 	initialise()
+
+	# import_from_status("Data//Status_example.csv")
